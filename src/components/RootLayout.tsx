0da31790f56b3f74a5266daa246ddc33e8d0d2ee
LRLayan <rameshlayan4@gmail.com>
2025-01-10 22:02:19 +0530

    modify root layout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -4,8 +4,14 @@ import {Outlet} from "react-router";
 export function RootLayout() {
     return(
         <>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className="flex flex-col h-screen bg-gray-800">
+                {/* Navbar at the top */}
+                <Navbar/>
+                {/* Main content area */}
+                <section className="absolute right-0 w-[78.5%] h-[97vh] overflow-y-auto p-5">
+                    <Outlet/>
+                </section>
+            </div>
         </>
     )
 }
