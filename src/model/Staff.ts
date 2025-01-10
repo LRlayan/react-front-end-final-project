bf1a56fa2a625724473126f8b33c45e3be760c00
LRLayan <rameshlayan4@gmail.com>
2025-01-10 22:19:35 +0530

    staff modal

---
+++ src/model/Staff.ts
@@ -0,0 +1,35 @@
+export class Staff{
+    code:string;
+    firstName:string;
+    lastName:string;
+    joinedDate:string;
+    designation:string;
+    gender:string;
+    dob:string;
+    addressLine01:string;
+    addressLine02:string;
+    addressLine03:string;
+    addressLine04:string;
+    addressLine05:string;
+    contactNo:string;
+    email:string;
+    role:string;
+
+    constructor(code: string, firstName: string, lastName: string, joinedDate: string, designation: string, gender: string, dob: string, addressLine01: string, addressLine02: string, addressLine03: string, addressLine04: string, addressLine05: string, contactNo: string, email: string, role: string) {
+        this.code = code;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.joinedDate = joinedDate;
+        this.designation = designation;
+        this.gender = gender;
+        this.dob = dob;
+        this.addressLine01 = addressLine01;
+        this.addressLine02 = addressLine02;
+        this.addressLine03 = addressLine03;
+        this.addressLine04 = addressLine04;
+        this.addressLine05 = addressLine05;
+        this.contactNo = contactNo;
+        this.email = email;
+        this.role = role;
+    }
+}
