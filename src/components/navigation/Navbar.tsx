d78a65d3f8e6ab793d1d26de9490d90e9f7201c4
LRLayan <rameshlayan4@gmail.com>
2025-01-10 22:02:49 +0530

    change color

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
             <header>
                 <div id="header-sec">
                     {/*Sidebar */}
-                    <div className="flex flex-col bg-gray-800 text-white h-screen fixed w-64 md:w-72">
+                    <div className="flex flex-col bg-gray-900 text-white h-screen fixed w-64 md:w-72">
                         <h3 className="text-center py-5 text-xl font-bold border-b border-gray-600">Dashboard</h3>
                         <nav className="flex-grow">
                             <Link to="/" className="block px-4 py-2 hover:bg-green-600">Dashboard</Link>
