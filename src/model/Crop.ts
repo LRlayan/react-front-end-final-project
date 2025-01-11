22707a6d24418e0615c71a352c894c2d7f9d0293
LRLayan <rameshlayan4@gmail.com>
2025-01-11 16:32:39 +0530

    add image property

--- src/model/Crop.ts
+++ src/model/Crop.ts
@@ -3,13 +3,15 @@ export class Crop{
     cropName:string;
     scientificName:string;
     category:string;
-    season:string
+    season:string;
+    image: File | null;
 
-    constructor(code: string, cropName: string, scientificName: string, category: string, season: string) {
+    constructor(code: string, cropName: string, scientificName: string, category: string, season: string, image: File | null) {
         this.code = code;
         this.cropName = cropName;
         this.scientificName = scientificName;
         this.category = category;
         this.season = season;
+        this.image = image
     }
 }
