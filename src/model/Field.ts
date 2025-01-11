222a7a281d46c8bcd4acd629956736dad31ebf1a
LRLayan <rameshlayan4@gmail.com>
2025-01-11 18:12:27 +0530

    add image property

--- src/model/Field.ts
+++ src/model/Field.ts
@@ -1,13 +1,17 @@
 export class Field{
     code:string;
     fieldName:string;
-    location:string
-    extentSize:number
+    location:string;
+    extentSize:number;
+    image1:File | null;
+    image2:File | null
 
-    constructor(code: string, fieldName: string, location: string, extentSize: number) {
+    constructor(code: string, fieldName: string, location: string, extentSize: number, image1: File | null, image2: File | null) {
         this.code = code;
         this.fieldName = fieldName;
         this.location = location;
         this.extentSize = extentSize;
+        this.image1 = image1
+        this.image2 = image2
     }
 }
