2160ab2632657d93222ddf2d4fc111f224b6421b
LRLayan <rameshlayan4@gmail.com>
2025-01-11 19:01:40 +0530

    fieldSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import CropSlice from "../reducer/CropSlice.ts";
+import FieldSlice from "../reducer/FieldSlice.ts";
 
 export const store = configureStore({
     reducer:{
         crop:CropSlice,
+        field:FieldSlice,
     }
 })
