1f7fd327f347c38c4650e0139769751b081dc203
LRLayan <rameshlayan4@gmail.com>
2025-01-11 23:47:37 +0530

    design layout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,17 +1,89 @@
-import {Navbar} from "./navigation/Navbar.tsx";
-import {Outlet} from "react-router";
+import {Layout, Menu, theme} from "antd";
+import {
+    AppstoreOutlined,
+    CarOutlined,
+    DashboardOutlined,
+    FieldTimeOutlined,
+    TeamOutlined,
+    ToolOutlined
+} from "@ant-design/icons";
+import React from "react";
 
-export function RootLayout() {
-    return(
-        <>
-            <div className="flex flex-col h-screen bg-gray-800">
-                {/* Navbar at the top */}
-                <Navbar/>
-                {/* Main content area */}
-                <section className="absolute right-0 w-[78.5%] h-[97vh] overflow-y-auto p-5">
-                    <Outlet/>
-                </section>
-            </div>
-        </>
-    )
-}
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [
+    {
+        key: '1',
+        icon: <DashboardOutlined />,
+        label: 'Dashboard',
+    },
+    {
+        key: '2',
+        icon: <AppstoreOutlined />,
+        label: 'Crop Manage',
+    },
+    {
+        key: '3',
+        icon: <FieldTimeOutlined />,
+        label: 'Field Manage',
+    },
+    {
+        key: '4',
+        icon: <TeamOutlined />,
+        label: 'Staff Manage',
+    },
+    {
+        key: '5',
+        icon: <ToolOutlined />,
+        label: 'Equipment Manage',
+    },
+    {
+        key: '6',
+        icon: <CarOutlined />,
+        label: 'Vehicle Manage',
+    },
+];
+
+const RootLayout: React.FC = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 442,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        content
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Crop Monitor System ©{new Date().getFullYear()} Created by Green Shadow
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default RootLayout;
