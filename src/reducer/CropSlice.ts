c3e1b680716930ea13d49a99c119de641cbede1d
LRLayan <rameshlayan4@gmail.com>
2025-01-11 19:00:41 +0530

    add semi colon

--- src/reducer/CropSlice.ts
+++ src/reducer/CropSlice.ts
@@ -14,6 +14,6 @@ const CropSlice = createSlice({
     }
 });
 
-export const{addCrop} = CropSlice.actions
-export default CropSlice.reducer
+export const{addCrop} = CropSlice.actions;
+export default CropSlice.reducer;
 
