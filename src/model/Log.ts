7f76da27439b0dc83c265800f3cc6385de0c59d6
LRLayan <rameshlayan4@gmail.com>
2025-01-11 23:47:50 +0530

    add new attribute

--- src/model/Log.ts
+++ src/model/Log.ts
@@ -2,10 +2,12 @@ export class Log{
     code:string;
     logDate:string;
     logDetails:string;
+    image:File | null
 
-    constructor(code: string, logDate: string, logDetails: string) {
+    constructor(code: string, logDate: string, logDetails: string, image:File | null) {
         this.code = code;
         this.logDate = logDate;
         this.logDetails = logDetails;
+        this.image = image
     }
 }
