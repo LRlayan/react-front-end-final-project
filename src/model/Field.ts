41cee32c180bd63323774775767aa0975421e87e
LRLayan <rameshlayan4@gmail.com>
2025-01-11 19:00:29 +0530

    image

--- src/model/Field.ts
+++ src/model/Field.ts
@@ -2,16 +2,14 @@ export class Field{
     code:string;
     fieldName:string;
     location:string;
-    extentSize:number;
-    image1:File | null;
-    image2:File | null
+    extentSize:string;
+    image:File | null;
 
-    constructor(code: string, fieldName: string, location: string, extentSize: number, image1: File | null, image2: File | null) {
+    constructor(code: string, fieldName: string, location: string, extentSize: string, image: File | null) {
         this.code = code;
         this.fieldName = fieldName;
         this.location = location;
         this.extentSize = extentSize;
-        this.image1 = image1
-        this.image2 = image2
+        this.image = image
     }
 }
