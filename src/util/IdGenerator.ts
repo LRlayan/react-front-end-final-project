47bc6e900b3c108239c9051043e693924e1d1a0f
LRLayan <rameshlayan4@gmail.com>
2025-01-14 03:03:50 +0530

    code generator

---
+++ src/util/IdGenerator.ts
@@ -0,0 +1,31 @@
+export class IdGenerator{
+
+    codeGenerator(type:string,code:string){
+
+        const increment = 1;
+
+        switch (type) {
+            case "CROP":
+                { 
+                    const parts = code.split('-');
+                    if (parts.length > 1) {
+                        const number = parseInt(parts[1]) + increment;
+                        return `CROP-${number}`;
+                    }
+                    return `CROP-1`;
+                }
+            case "FIELD":
+                return "";
+            case "LOG":
+                return "";
+            case "EQUIPMENT":
+                return "";
+            case "STAFF":
+                return '';
+            case "VEHICLE":
+                return '';
+            default:
+                return increment
+        }
+    }
+}
