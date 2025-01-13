a2ab7c5748011de4e167b06c21ba673ce5077aa7
LRLayan <rameshlayan4@gmail.com>
2025-01-13 15:50:39 +0530

    updateCrop

--- src/reducer/CropSlice.ts
+++ src/reducer/CropSlice.ts
@@ -10,10 +10,17 @@ const CropSlice = createSlice({
     reducers:{
         addCrop:(state,action)=>{
             state.crops.push(action.payload);
+        },
+        updateCrop:(state,action)=>{
+            // @ts-ignore
+            const index = state.crops.findIndex(c => c.code === action.payload.code);
+            if (index !== -1) {
+                state.crops[index] = action.payload;
+            }
         }
     }
 });
 
-export const{addCrop} = CropSlice.actions;
+export const{addCrop,updateCrop} = CropSlice.actions;
 export default CropSlice.reducer;
 
