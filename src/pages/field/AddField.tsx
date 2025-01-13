3ec6b8c4d7abc7207e2257da13022cde6080e507
LRLayan <rameshlayan4@gmail.com>
2025-01-14 02:11:38 +0530

    complete add field

--- src/pages/field/AddField.tsx
+++ src/pages/field/AddField.tsx
@@ -1,8 +1,64 @@
 import React, { useState } from "react";
+import {useDispatch} from "react-redux";
+import {Field} from "../../model/Field.ts";
+import {addField} from "../../reducer/FieldSlice.ts";
+import MainModal from "../../components/modal/MainModal.tsx";
 
-const AddField: React.FC<{isOpen: boolean; onClose: () => void}> = ({isOpen, inClose }) => {
+const AddField: React.FC<{isOpen: boolean; onClose: () => void}> = ({isOpen, onClose }) => {
+
+    const dispatch = useDispatch();
+    const [code, setCode] = useState("");
+    const [fieldName, setFieldName] = useState("");
+    const [location, setLocation] = useState("");
+    const [extentSize, setExtentSize] = useState("");
+    const [image, setImage] = useState<File | null>(null);
+
+    const handleSubmit = () => {
+        const newField = new Field(code,fieldName,location,extentSize,image);
+        dispatch(addField(newField));
+        onClose();
+    }
+
     return(
-        <></>
+        <>
+            <MainModal isType="ADD FIELD" buttonType={"Save"} isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
+                <form>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-50">Field Name</label>
+                        <input
+                            type="text"
+                            className="mt-1 block w-full px-4 py-1 border rounded-md shadow-sm"
+                            onChange={(e) => setFieldName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-50">Location</label>
+                        <input
+                            type="text"
+                            className="mt-1 block w-full px-4 py-1 border rounded-md shadow-sm"
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-50">Extent Size</label>
+                        <input
+                            type="text"
+                            className="mt-1 block w-full px-4 py-1 border rounded-md shadow-sm"
+                            onChange={(e) => setExtentSize(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-50">Field Image</label>
+                        <input
+                            type="file"
+                            accept="image/*"
+                            className="mt-1 block w-full px-4 py-1 border rounded-md shadow-sm"
+                            onChange={(e) => setImage(e.target.files ? e.target.files[0] : null)}
+                        />
+                    </div>
+                </form>
+            </MainModal>
+        </>
     )
 }
 
