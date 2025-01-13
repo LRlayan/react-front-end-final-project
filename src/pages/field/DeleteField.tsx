ec76301a491853185aca8af879389ff69c49b3ec
LRLayan <rameshlayan4@gmail.com>
2025-01-14 01:31:31 +0530

    DeleteField.tsx

---
+++ src/pages/field/DeleteField.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const DeleteField: React.FC<{isOpen: boolean; onClose: () => void}> = ({isOpen,onClose}) => {
+    return(
+        <></>
+    )
+}
+
+export default DeleteField;
