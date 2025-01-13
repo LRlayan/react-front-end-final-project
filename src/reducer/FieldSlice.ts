74f65f532ccda213eba7f872cfa826ebda4ac007
LRLayan <rameshlayan4@gmail.com>
2025-01-14 01:35:31 +0530

    UpdateField in slice

--- src/reducer/FieldSlice.ts
+++ src/reducer/FieldSlice.ts
@@ -10,7 +10,14 @@ const FieldSlice = createSlice({
    reducers:{
        addField:(state,action) =>{
            state.fields.push(action.payload);
-       }
+       },
+       updateField:(state,action) => {
+           // @ts-ignore
+           const index = state.fields.findIndex(f => f.code === action.payload.code);
+           if (index !== -1) {
+               state.fields[index] = action.payload;
+           }
+       },
    }
 });
 
