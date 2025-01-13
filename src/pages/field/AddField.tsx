6f8989599d7832b58b1a394dc9b1987614a9bc38
LRLayan <rameshlayan4@gmail.com>
2025-01-14 01:31:36 +0530

    AddField.tsx

---
+++ src/pages/field/AddField.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const AddField: React.FC<{isOpen: boolean; onClose: () => void}> = ({isOpen, inClose }) => {
+    return(
+        <></>
+    )
+}
+
+export default AddField;
