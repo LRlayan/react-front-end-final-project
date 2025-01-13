9b84f05196d6e45d6b10f04e12ff6ab53e7716e9
LRLayan <rameshlayan4@gmail.com>
2025-01-14 01:31:43 +0530

    UpdateField.tsx

---
+++ src/pages/field/UpdateField.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const UpdateField: React.FC<{isOpen: boolean; onClose: () => void}> = ({ isOpen, onClose }) =>{
+    return(
+        <></>
+    )
+}
+
+export default UpdateField;
