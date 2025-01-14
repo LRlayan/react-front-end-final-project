53c97e705e5b920ebe81bfd05c6af04e71ffe7f8
LRLayan <rameshlayan4@gmail.com>
2025-01-14 15:44:59 +0530

    AddLog.tsx

---
+++ src/pages/log/AddLog.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+const AddLog: React.FC<{isOpen: boolean; onClose: () => void}> = ({ isOpen, onClose }) => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default AddLog;
