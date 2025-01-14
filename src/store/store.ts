053a80c2cc6cce74c35a9398396fd74a19638767
LRLayan <rameshlayan4@gmail.com>
2025-01-14 16:56:24 +0530

    setLogSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import CropSlice from "../reducer/CropSlice.ts";
 import FieldSlice from "../reducer/FieldSlice.ts";
+import LogSlice from "../reducer/LogSlice.ts";
 
 export const store = configureStore({
     reducer:{
         crop:CropSlice,
         field:FieldSlice,
+        log:LogSlice,
     }
 })
