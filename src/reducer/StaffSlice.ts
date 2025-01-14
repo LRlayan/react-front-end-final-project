af4fb95075e512d9d9ae766e7441f20baac0b783
LRLayan <rameshlayan4@gmail.com>
2025-01-15 00:00:57 +0530

    update staff

--- src/reducer/StaffSlice.ts
+++ src/reducer/StaffSlice.ts
@@ -10,9 +10,16 @@ const StaffSlice = createSlice({
     reducers:{
         addStaff:(state,action)=>{
             state.staffs.push(action.payload);
-        }
+        },
+        updateStaff:(state,action)=>{
+            //@ts-ignore
+            const index = state.staffs.findIndex(s => s.code === action.payload.code);
+            if (index !== -1) {
+                state.staffs[index] = action.payload;
+            }
+        },
     }
 })
 
-export const {addStaff} = StaffSlice.actions;
+export const {addStaff,updateStaff} = StaffSlice.actions;
 export default StaffSlice.reducer;
