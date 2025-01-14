7a51722cc8d55a36a73e59c95323df9b61fd2e6a
LRLayan <rameshlayan4@gmail.com>
2025-01-15 00:03:26 +0530

    setStaffSlice for store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import CropSlice from "../reducer/CropSlice.ts";
 import FieldSlice from "../reducer/FieldSlice.ts";
 import LogSlice from "../reducer/LogSlice.ts";
+import StaffSlice from "../reducer/StaffSlice.ts";
 
 export const store = configureStore({
     reducer:{
         crop:CropSlice,
         field:FieldSlice,
         log:LogSlice,
+        staff:StaffSlice,
     }
 })
