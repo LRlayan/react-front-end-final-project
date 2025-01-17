9128fe9356520f25179b6a087df4024807b3c00b
LRLayan <rameshlayan4@gmail.com>
2025-01-17 17:28:58 +0530

    delete vehicle

---
+++ src/pages/vehicle/DeleteVehicle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Vehicle} from "../../model/Vehicle.ts";
+
+const DeleteVehicle: React.FC<{ isOpen:boolean; onClose: () => void; vehicles: Vehicle; isType:string; buttonType:string} = ({ isOpen, onClose, vehicles, isType, buttonType }) => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default DeleteVehicle;
