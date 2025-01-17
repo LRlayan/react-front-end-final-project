8118fd30b5da994b4b47db90d1b9cf9456ba642e
LRLayan <rameshlayan4@gmail.com>
2025-01-17 19:31:44 +0530

    add Equipment

---
+++ src/pages/equipment/EquipmentPage.tsx
@@ -0,0 +1,133 @@
+import Search from "antd/es/input/Search";
+import {Button} from "antd";
+import {PlusCircleOutlined} from "@ant-design/icons";
+import React, {useState} from "react";
+import {useSelector} from "react-redux";
+import {RootState} from "../../reducer/EquipmentSlice.ts";
+import {Equipment} from "../../model/Equipment.ts";
+import Table from "../../components/table/Table.tsx";
+import {Vehicle} from "../../model/Vehicle.ts";
+import AddEquipment from "./AddEquipment.tsx";
+
+interface EquipmentDataType {
+    key: React.Key;
+    code: string;
+    name: string;
+    type: string;
+    status: string;
+    count: number;
+}
+
+const EquipmentPage = () => {
+
+    const [open, setOpen] = useState(false);
+    const [modalType, setModalType] = useState("");
+    const [selectedEquipment, setSelectedEquipment] = useState<Equipment | null>(null);
+    const equipment = useSelector((state:RootState) => state.equipment.equipments) || [];
+
+    const openAddModal = () => {
+        setOpen(true);
+        setModalType("add");
+    }
+
+    const columns = [
+        {
+            title:"Code",
+            dataIndex:"code",
+            fixed:"left",
+            key:"code"
+        },
+        {
+            title:"Equipment Name",
+            dataIndex:"name",
+            fixed:"left",
+            key:"name"
+        },
+        {
+            title:"Type",
+            dataIndex:"type",
+            key:"type"
+        },
+        {
+            title:"Status",
+            dataIndex:"status",
+            key:"status"
+        },
+        {
+            title:"Count",
+            dataIndex:"count",
+            key:"count"
+        },
+        {
+            title: 'Action 1',
+            key: 'update',
+            fixed: 'right',
+            render: (_:Vehicle, record: EquipmentDataType) => (
+                <Button
+                    className="text-blue-500"
+                    type="link"
+                    onClick={() => {
+                        openUpdateModal(record)
+                    }}
+                >
+                    Edit
+                </Button>
+            )
+        },
+        {
+            title: 'Action 2',
+            key: 'delete',
+            fixed: 'right',
+            render: (_:Vehicle, record: EquipmentDataType) => (
+                <Button
+                    className="text-red-500"
+                    type="link"
+                    onClick={() => {
+                        openDeleteModal(record)
+                    }}
+                >
+                    Delete
+                </Button>
+            )
+        }
+    ];
+
+    return(
+        <>
+            <section id="staff-sec" className="mt-4 p-6">
+                <div className="container mx-auto">
+                    <div className="flex justify-between items-center mb-4">
+                        <Search className="mr-5" placeholder="Search staff by name or role" enterButton/>
+                        <Button
+                            type="primary"
+                            icon={<PlusCircleOutlined/>}
+                            className="btn bg-green-500 hover:bg-green-600 text-white"
+                            onClick={() => openAddModal()}
+                        >
+                            New
+                        </Button>
+                    </div>
+                </div>
+                <Table<Equipment>
+                    columns={columns}
+                    dataSource={equipment.map((equipment) => ({
+                        ...equipment,
+                        key: equipment.code,
+                    }))}
+                />
+                {open && modalType === "add" && (
+                    <AddEquipment
+                        isOpen={open}
+                        isType={"ADD EQUIPMENT"}
+                        buttonType={"Save"}
+                        onClose={() => {
+                            setOpen(false);
+                        }}
+                    />
+                )}
+            </section>
+        </>
+    )
+}
+
+export default EquipmentPage;
