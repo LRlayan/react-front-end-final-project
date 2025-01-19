9a82b91bdd77860486cddb3db3b34cd109e0c166
LRLayan <rameshlayan4@gmail.com>
2025-01-19 21:18:46 +0530

    assign fields

--- src/pages/crop/UpdateCrop.tsx
+++ src/pages/crop/UpdateCrop.tsx
@@ -1,10 +1,13 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import React, {useEffect, useState} from "react";
 import {Crop} from "../../model/Crop.ts";
 import {updateCrop} from "../../reducer/CropSlice.ts";
 import MainModal from "../../components/modal/MainModal.tsx";
 import Label from "../../components/label/Label.tsx";
-import {Input} from "antd";
+import {Input, Select, SelectProps, Tag} from "antd";
+import {Field} from "../../model/Field.ts";
+import {FieldRootState} from "../../reducer/FieldSlice.ts";
+import tagRender from "../../util/TagRender.tsx";
 
 
 const UpdateCrop: React.FC<{ isOpen: boolean; onClose: () => void; crop:Crop; isType:string; buttonType:string}> = ({isOpen, onClose, crop, isType, buttonType}) => {
@@ -16,6 +19,8 @@ const UpdateCrop: React.FC<{ isOpen: boolean; onClose: () => void; crop:Crop; is
     const [category, setCategory] = useState("");
     const [season, setSeason] = useState("");
     const [image, setImage] = useState<File | null>(null);
+    const [fields, setFields] = useState<Field[]>([]);
+    const field = useSelector((state:FieldRootState) => state.field.fields);
 
     useEffect(() => {
         if (crop) {
@@ -25,11 +30,17 @@ const UpdateCrop: React.FC<{ isOpen: boolean; onClose: () => void; crop:Crop; is
             setCategory(crop.category);
             setSeason(crop.season);
             setImage(crop.image);
+            setFields(crop.assignFields);
         }
     }, [crop]);
 
+    const fieldOptions: SelectProps['options'] = field.map((field) => ({
+        label: field.name,
+        value: field.code
+    }));
+
     const handleSubmit = () => {
-        const updateCropDetails = new Crop(cropCode, cropName, scientificName, category, season, image);
+        const updateCropDetails = new Crop(cropCode, cropName, scientificName, category, season, image, fields);
         dispatch(updateCrop(updateCropDetails))
     }
 
@@ -67,6 +78,35 @@ const UpdateCrop: React.FC<{ isOpen: boolean; onClose: () => void; crop:Crop; is
                             type="text"
                             className="mt-1 block w-full px-4 py-1 border rounded-md shadow-sm"
                             onChange={(e) => setSeason(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4 custom-input">
+                        <Label labelName={"Assign Field"}/>
+                        <Select
+                            mode="multiple"
+                            tagRender={tagRender}
+                            style={{
+                                width: '100%',
+                                color: 'black',
+                            }}
+                            options={fieldOptions}
+                            dropdownStyle={{
+                                backgroundColor: 'white',
+                            }}
+                            dropdownClassName="custom-dropdown"
+                            onChange={(selectedValues) => {
+                                const selectedFields = selectedValues.map((value: string) => {
+                                    const matchedField = field.find((f) => f.code === value);
+                                    return matchedField
+                                        ? {
+                                            ...matchedField,
+                                            fieldName: matchedField.name,
+                                        }
+                                        : null;
+                                });
+                                const validFields = selectedFields.filter((f: Field) => f !== null);
+                                setFields(validFields as Field[]);
+                            }}
                         />
                     </div>
                     <div className="mb-4 custom-input">
