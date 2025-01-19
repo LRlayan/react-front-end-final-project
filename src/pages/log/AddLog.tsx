47c7e6cf078c04691a3cb7fed04bb31e9d7651fa
LRLayan <rameshlayan4@gmail.com>
2025-01-19 21:16:52 +0530

    assign crops

--- src/pages/log/AddLog.tsx
+++ src/pages/log/AddLog.tsx
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Log} from "../../model/Log.ts";
-import {addLog, RootState} from "../../reducer/LogSlice.ts";
+import {addLog, LogRootState} from "../../reducer/LogSlice.ts";
 import {IdGenerator} from "../../util/IdGenerator.ts";
 import MainModal from "../../components/modal/MainModal.tsx";
-import {Input} from "antd";
+import {Input, Select, SelectProps} from "antd";
 import Label from "../../components/label/Label.tsx";
+import tagRender from "../../util/TagRender.tsx";
+import {Crop} from "../../model/Crop.ts";
+import {CropRootState} from "../../reducer/CropSlice.ts";
 
 const AddLog: React.FC<{isOpen: boolean; onClose: () => void; isType:string; buttonType:string}> = ({ isOpen, onClose, isType, buttonType }) => {
 
@@ -14,14 +17,20 @@ const AddLog: React.FC<{isOpen: boolean; onClose: () => void; isType:string; but
     const [lodDate, setLogDate] = useState("");
     const [logDetails, setLogDetails] = useState("");
     const [image, setImage] = useState<File | null>(null);
-    const logs = useSelector((state:RootState) => state.log.logs);
-
+    const [selectedCrops, setCrops] = useState<Crop[]>([]);
+    const logs = useSelector((state:LogRootState) => state.log.logs);
+    const crops = useSelector((state:CropRootState) => state.crop.crops);
     const idGenerator = new IdGenerator();
+
+    const cropOptions: SelectProps['options'] = crops.map((crop) => ({
+        label: crop.name,
+        value: crop.code
+    }));
 
     function handleSubmit() {
         const getLastLogCode = logs.length > 0 ? logs[logs.length -1].code : "LOG-";
         const newCode = idGenerator.codeGenerator("LOG",getLastLogCode);
-        const newLogs = new Log(newCode,logName,lodDate,logDetails,image);
+        const newLogs = new Log(newCode,logName,lodDate,logDetails,image,selectedCrops);
         dispatch(addLog(newLogs));
         onClose();
     }
@@ -52,6 +61,35 @@ const AddLog: React.FC<{isOpen: boolean; onClose: () => void; isType:string; but
                             type="text"
                             className="mt-1 block w-full px-4 py-1 border rounded-md shadow-sm"
                             onChange={(e) => setLogDetails(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4 custom-input">
+                        <Label labelName={"Assign Crops"}/>
+                        <Select
+                            mode="multiple"
+                            tagRender={tagRender}
+                            style={{
+                                width: '100%',
+                                color: 'black',
+                            }}
+                            options={cropOptions}
+                            dropdownStyle={{
+                                backgroundColor: 'white',
+                            }}
+                            dropdownClassName="custom-dropdown"
+                            onChange={(selectedValues) => {
+                                const selectedCrops = selectedValues.map((value: string) => {
+                                    const matchedCrop = crops.find((c) => c.code === value);
+                                    return matchedCrop
+                                        ? {
+                                            ...matchedCrop,
+                                            cropName: matchedCrop.name,
+                                        }
+                                        : null;
+                                });
+                                const validCrops = selectedCrops.filter((c: Crop) => c !== null);
+                                setCrops(validCrops as Crop[]);
+                            }}
                         />
                     </div>
                     <div className="mb-4 custom-input">
