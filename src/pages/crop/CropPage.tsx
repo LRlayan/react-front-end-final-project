64838af0b42681021393185a9a09bbb1bdbd5262
LRLayan <rameshlayan4@gmail.com>
2025-01-19 18:33:50 +0530

    call Card component

--- src/pages/crop/CropPage.tsx
+++ src/pages/crop/CropPage.tsx
@@ -9,6 +9,7 @@ import DeleteCrop from "./DeleteCrop.tsx";
 import {Crop} from "../../model/Crop.ts";
 import SearchingTableData from "../../util/SearchingTableData.ts";
 import {CropRootState} from "../../reducer/CropSlice.ts";
+import Card from "../../components/card/Card.tsx";
 
 const CropPage = () => {
     const [open, setOpen] = useState(false);
@@ -59,43 +60,7 @@ const CropPage = () => {
                     </Button>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {filteredCrop.map((crop:Crop, index:number) => (
-                        <div key={index} className="border rounded-lg bg-gray-700 text-white p-4 shadow-md">
-                            {crop.image && (
-                                <img
-                                    src={URL.createObjectURL(crop.image)}
-                                    alt={crop.cropName}
-                                    className="w-full h-32 object-cover rounded-md mb-2"
-                                />
-                            )}
-                            <h4 className="text-lg font-semibold">{crop.code}</h4>
-                            <p className="text-sm">Scientific Name: {crop.cropName}</p>
-                            <p className="text-sm">Scientific Name: {crop.scientificName}</p>
-                            <p className="text-sm">Category: {crop.category}</p>
-                            <p className="text-sm">Season: {crop.season}</p>
-                            <p className="text-sm">Fields: {crop.assignFields.map((f) => f.fieldName).join(", ")}</p>
-                            <div className="flex space-x-2 mt-2">
-                                {/* Update Button */}
-                                <Button
-                                    type="primary"
-                                    className="btn bg-green-500 hover:bg-green-600 text-white"
-                                    style={{width: '140px'}}
-                                    onClick={() => openUpdateModal(crop)}
-                                >
-                                    Update
-                                </Button>
-
-                                {/* Delete Button */}
-                                <Button
-                                    className="btn bg-red-500 hover:bg-red-600 text-white border-red-500"
-                                    style={{width: '140px'}}
-                                    onClick={() => openDeleteModal(crop)}
-                                >
-                                    Delete
-                                </Button>
-                            </div>
-                        </div>
-                    ))}
+                    <Card filteredCrop={filteredCrop} openUpdateModal={openUpdateModal} openDeleteModal={openDeleteModal}/>
                 </div>
             </div>
             {open && modalType === "add" && (
