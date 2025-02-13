f7a0d5f7d5cbda2eac7f447c7d23c8a3b7e4b9d1
LRLayan <rameshlayan4@gmail.com>
2025-02-13 16:46:50 +0530

    saveLog()

--- src/reducer/LogSlice.ts
+++ src/reducer/LogSlice.ts
@@ -1,9 +1,11 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {Crop} from "../model/Crop.ts";
 import {Staff} from "../model/Staff.ts";
 import {Field} from "../model/Field.ts";
+import {api} from "../api/api.ts";
+import {Log} from "../model/Log.ts";
 
-const initialState = {
+const initialState: { logs: Log[] } = {
     logs:[]
 }
 
@@ -22,26 +24,41 @@ export type LogRootState = {
     };
 };
 
+export const saveLog = createAsyncThunk(
+    'log/saveLog',
+    async (log: FormData, { dispatch })=> {
+        try {
+            const response = await api.post('log/saveLog', log, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+            return response.data;
+        } catch (e) {
+            console.error("Failed to save logs!", e);
+            throw e;
+        }
+    }
+);
+
 const LogSlice = createSlice({
     name:"log",
     initialState:initialState,
-    reducers:{
-        addLog:(state,action) => {
-            state.logs.push(action.payload);
-        },
-        updateLog:(state,action) => {
-            //@ts-ignore
-            const index = state.logs.findIndex(l => l.code === action.payload.code);
-            if (index !== -1) {
-                state.logs[index] = action.payload;
-            }
-        },
-        deleteLog:(state,action) => {
-            //@ts-ignore
-            state.logs = state.logs.filter(l => l.code !== action.payload.code);
-        }
+    reducers:{},
+    extraReducers: (builder) => {
+        builder
+            .addCase(saveLog.fulfilled, (state, action) => {
+                if (action.payload) {
+                    state.logs = [...state.logs, action.payload];
+                }
+            })
+            .addCase(saveLog.pending, () => {
+                console.error("Pending save logs");
+            })
+            .addCase(saveLog.rejected, () => {
+                console.error("Rejected save logs")
+            })
     }
-})
+});
 
-export const {addLog,updateLog,deleteLog} = LogSlice.actions;
 export default LogSlice.reducer;
