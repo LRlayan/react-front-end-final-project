07c060ebe5a7ed769dd8d021d7ee4651f5c4fefd
LRLayan <rameshlayan4@gmail.com>
2025-02-13 17:05:02 +0530

    updateLogs()

--- src/reducer/LogSlice.ts
+++ src/reducer/LogSlice.ts
@@ -42,6 +42,24 @@ export const saveLog = createAsyncThunk(
     }
 );
 
+export  const updateLog = createAsyncThunk(
+    'log/updateLog',
+    async (log : FormData, { dispatch }) => {
+        try {
+            const response = await api.put(`log/updateLog/${log.get("code")}`, log,{
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+            dispatch(getAllLogs());
+            return response.data;
+        } catch (e) {
+            console.error("Failed to update logs!", e);
+            throw e;
+        }
+    }
+);
+
 export const getAllLogs = createAsyncThunk(
     'logs/getAllLogs',
     async () => {
@@ -71,6 +89,26 @@ const LogSlice = createSlice({
             })
             .addCase(saveLog.rejected, () => {
                 console.error("Rejected save logs")
+            })
+            .addCase(updateLog.fulfilled, (state,action) => {
+                const formData = action.meta.arg;
+                const code = formData.get("code");
+
+                if (!code) {
+                    console.error("Error: No code in FormData!");
+                    return;
+                }
+
+                const index = state.logs.findIndex(l => l.code);
+                if (index !== -1) {
+                    state.logs[index] = action.payload;
+                }
+            })
+            .addCase(updateLog.pending, () => {
+                console.error("Pending update logs");
+            })
+            .addCase(updateLog.rejected, () => {
+                console.error("Rejected update logs");
             })
             .addCase(getAllLogs.fulfilled, (state, action) => {
                 state.logs = action.payload || [];
