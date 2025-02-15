442e9662528ca565f6f3f92be76d8bff5896a840
LRLayan <rameshlayan4@gmail.com>
2025-02-15 13:03:47 +0530

    saveVehicle

--- src/reducer/VehicleSlice.ts
+++ src/reducer/VehicleSlice.ts
@@ -1,7 +1,9 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {Staff} from "../model/Staff.ts";
+import {Vehicle} from "../model/Vehicle.ts";
+import {api} from "../api/api.ts";
 
-const initialState = {
+const initialState: { vehicles: Vehicle[] } = {
     vehicles:[]
 }
 
@@ -15,31 +17,40 @@ export type VehicleRootState = {
            fuelType: string;
            status: string;
            remark: string;
-            assignStaffMember: Staff;
-        }>;
+           assignStaffMember: Staff;
+        }>
     }
 };
 
+export const saveVehicle = createAsyncThunk(
+    'vehicle/saveVehicle',
+    async (vehicle: Vehicle) => {
+        try {
+            const response = await api.post("vehicle/saveVehicle", vehicle);
+            return response.data;
+        } catch (e) {
+            console.error("Failed to save vehicle!", e);
+            throw e;
+        }
+    }
+);
+
 const VehicleSlice = createSlice({
     name:"vehicle",
-    initialState:initialState,
-    reducers: {
-        addVehicle:(state,action) => {
-            state.vehicles.push(action.payload);
-        },
-        updateVehicle:(state,action) => {
-            //@ts-ignore
-            const index = state.vehicles.findIndex(v => v.code === action.payload.code)
-            if (index !== -1) {
-                state.vehicles[index] = action.payload;
-            }
-        },
-        deleteVehicle:(state,action) => {
-            //@ts-ignore
-            state.vehicles = state.vehicles.filter(v => v.code !== action.payload.code);
-        }
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(saveVehicle.fulfilled, (state, action) => {
+                state.vehicles = [...state.vehicles, action.payload];
+            })
+            .addCase(saveVehicle.pending, () => {
+                console.error("Pending save vehicle");
+            })
+            .addCase(saveVehicle.rejected, () => {
+                console.error("Rejected save vehicle");
+            })
     }
 });
 
-export const {addVehicle,updateVehicle,deleteVehicle} = VehicleSlice.actions
 export default VehicleSlice.reducer;
