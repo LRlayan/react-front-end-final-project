afb6f4451c59e322b0ef6135edf75a5d3b6ad504
LRLayan <rameshlayan4@gmail.com>
2025-02-15 19:29:32 +0530

    count card

---
+++ src/components/dashboard/count-card/CountCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface CountCardProps {
+    countCardType: string;
+    count: number;
+}
+
+const CountCard: React.FC<CountCardProps> = ({
+    countCardType,
+    count
+}) => {
+    return(
+        <>
+             <h5 className="text-lg font-medium">{countCardType}</h5>
+             <h2 id="employees" className="text-4xl font-bold">{count}</h2>
+        </>
+    )
+}
+
+export default CountCard;
