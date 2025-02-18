5906298ff52b40c6925db214dd37f70d338b40c3
LRLayan <rameshlayan4@gmail.com>
2025-02-18 19:32:11 +0530

    loginUser()

--- src/reducer/UserSlice.ts
+++ src/reducer/UserSlice.ts
@@ -2,30 +2,29 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {api} from "../api/api.ts";
 import {User} from "../model/User.ts";
 
-const initialState : {user: User[], jwt_token: null, refresh_token: null, username: null, isAuthenticated: false, loading: false, error: string } = {
-    user : [],
+const initialState : { user: User | null, jwt_token: null, refresh_token: null, username: null, isAuthenticated: boolean, loading: false, error: string } = {
+    user: null,
     jwt_token: null,
     refresh_token: null,
     username: null,
     isAuthenticated: false,
     loading: false,
     error: ""
-}
+};
+
 
 export type UserRootState = {
     user: {
-        users: Array<{
-            username: string;
-            email: string;
-            password: string;
-            jwt_token: null,
-            refresh_token: null,
-            isAuthenticated: false,
-            loading: false,
-            error: ""
-        }>
-    }
-}
+        user: User | null;
+        jwt_token: null;
+        refresh_token: null;
+        username: null;
+        isAuthenticated: boolean;
+        loading: boolean;
+        error: string;
+    };
+};
+
 
 export const registerUser = createAsyncThunk(
     'user/registerUser',
@@ -44,15 +43,32 @@ export const registerUser = createAsyncThunk(
     }
 );
 
+export const loginUser = createAsyncThunk(
+    'user/loginUser',
+    async (user: User) => {
+        try {
+            const response = await api.post(
+                'auth/login',
+                {user},
+                {withCredentials: true}
+            );
+            return response.data;
+        } catch (e) {
+            throw e;
+        }
+    }
+);
+
 const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(registerUser.fulfilled, (state,action) => {
+            .addCase(registerUser.fulfilled, (state, action) => {
                 if (action.payload) {
-                    state.user = [...state.user, action.payload];
+                    state.user = action.payload;
+                    state.isAuthenticated = true;
                 }
             })
             .addCase(registerUser.pending, () => {
