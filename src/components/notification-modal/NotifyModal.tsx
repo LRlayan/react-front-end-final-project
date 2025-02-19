6498ad5275a1d44b9d7af34855886f39dab765eb
LRLayan <rameshlayan4@gmail.com>
2025-02-20 02:02:28 +0530

    logout() and remove token

--- src/components/notification-modal/NotifyModal.tsx
+++ src/components/notification-modal/NotifyModal.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Modal } from 'antd';
 import {useNavigate} from "react-router";
+import {logout} from "../../reducer/UserSlice.ts";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../store/store.ts";
 
 interface NotifyProps {
     isOpen: boolean;
@@ -10,12 +13,15 @@ interface NotifyProps {
 const App: React.FC<NotifyProps> = ({isOpen, onClose}) => {
     const [confirmLoading, setConfirmLoading] = useState(false);
     const navigate = useNavigate();
+    const dispatch = useDispatch<AppDispatch>();
 
     const handleOk = () => {
         setConfirmLoading(true);
         setTimeout(() => {
             setConfirmLoading(false);
             navigate("/");
+            localStorage.removeItem("jwt_token")
+            dispatch(logout());
             onClose();
         }, 700);
     };
